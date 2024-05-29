788ec491698e69eddb32793daa1ec5c158fbd24d
Dhruv <pateldhruv5536@gmail.com>
2024-05-29 09:56:36 +0530

    varibles var let const

---
+++ 01_basic/varibles.js
@@ -0,0 +1,17 @@
+const accountId = 101;
+// accountId=102  ====> error  Assignment to constant variable.
+// not allowed accountId = 102 ; after declare one time
+
+let accountEmail = "abc@gmail.com";
+// let is use only for scope if (condition) { scope  }
+
+var accountPassword ="1245"
+//globelvaribles
+
+console.log(accountId)
+
+accountCity = "surat ---> error "
+
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
