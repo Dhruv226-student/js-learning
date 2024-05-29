b0c52b2c280620362a7b67d3f126ebf61175b89b
Dhruv <pateldhruv5536@gmail.com>
2024-05-29 17:12:21 +0530

    strings

---
+++ 01_basic/5_string.js
@@ -0,0 +1,31 @@
+// const name = "Dhruv";
+
+// const repocount = 50;
+// //console.log(name + repocount + "value");
+// console.log(`hello my name is ${name} my repo count is ${repocount}`);
+
+
+const gameName = new String('dhruv Krishna')
+console.log(gameName.split(" ")) // [ 'dhruv', 'Krishna' ]
+
+// console.log(gameName[0])
+// console.log(gameName.__proto__)
+
+
+
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+// console.log(gameName.charAt())
+const newString = gameName.substring(0,5)//strat num  and end num
+// console.log(newString); //dhruv
+
+
+
+// const anotherSting = gameName.slice(0,7)
+// console.log(anotherSting)
+
+
+// const newStringOne ="       hehgfcsh         " //trim 
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+// console.log(newString.replace("","%"))
