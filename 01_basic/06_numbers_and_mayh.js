2a4c39b4ea40e1c2869aa9d8acaafc198c7f0885
Dhruv <pateldhruv5536@gmail.com>
2024-05-29 17:36:47 +0530

    math and num

---
+++ 01_basic/06_numbers_and_mayh.js
@@ -0,0 +1,19 @@
+const score = 5000
+// console.log(score); // => 5000
+const balance =new Number(100)
+// console.log(balance) // => [Number: 100]
+// console.log(balance.toFixed(2)) // => 100.00
+
+const hundres= 1000000
+// console.log(hundres.toLocaleString('en-IN'))
+
+/* *************** Maths*/
+
+console.log(Math.abs(-4));
+console.log((Math.random()*10)+1)
+
+
+const min =10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)) + min)
