e67ce0b89f4a6991e1bce4b23161f4be8dee6447
Dhruv <pateldhruv5536@gmail.com>
2024-05-29 11:11:19 +0530

    operation

--- 01_basic/conversionOperator.js
+++ 01_basic/conversionOperator.js
@@ -1,11 +1,11 @@
 let score = "33abc";
 
-console.log(typeof score);
+// console.log(typeof score);
 
 //coversion
 let valueInNUmber = Number(score)
-console.log(typeof valueInNUmber);
-console.log(valueInNUmber); // output >>> not a number nan
+// console.log(typeof valueInNUmber);
+// console.log(valueInNUmber); // output >>> not a number nan
 
 // "33" => 33
 //"33abc" => nan
@@ -14,8 +14,43 @@ console.log(valueInNUmber); // output >>> not a number nan
 
 let isLoggedIn = "hero";
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
+// console.log(typeof stringNumber)
+
+//**********************Operation ********************** */
+let value = 3
+let negvalue = -value
+// console.log(negvalue) //=> output  -3
+
+let str1 ="heloo"
+let str2 =" world"
+
+str3= str1+str2
+// console.log(str3)
+
+// //number conversion confusing
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2) //output 122
+// console.log(1+2+"2") //output 32
+
+let gameCounter =100;
+gameCounter++
+
+console.log(gameCounter);
+//pre fix ++a
+//post fix  b++
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
