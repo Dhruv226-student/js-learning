96b95cffd71e489e294d9695a4e030f0c25d6904
Dhruv <pateldhruv5536@gmail.com>
2024-05-31 12:11:23 +0530

    arrow function

---
+++ 03_basic/03_arrow.js
@@ -0,0 +1,44 @@
+const user = {
+    username :"dhruv",
+    price :500,
+
+    welcomeMessage: function(){
+        console.log(`${this.username} ,welcome to website `)
+        console.log(this)
+    }
+}
+
+// user.welcomeMessage()
+// user.username ="dhruvika"
+// user.welcomeMessage()
+// console.log(this) // current content {} empty object
+
+
+//arrow function
+function chai(){
+    // console.log(this)
+}
+// chai()
+const one =()=>{
+    // let username ="ddhruv"
+    // console.log(this.username)
+
+
+
+}
+// one()
+
+// const addTwo=(num1,num2)=>{
+//     return num1 + num2
+
+// }
+// console.log(addTwo(6,9))
+
+// const addTwo=(num1,num2)=> num1 + num2  //inprsit retuen 
+
+
+console.log(addTwo(6,9))
+
+
+
+
