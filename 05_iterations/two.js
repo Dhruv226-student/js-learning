e1567e27710019dc0c55772b852fef742e74e204
Dhruv <pateldhruv5536@gmail.com>
2024-05-31 17:30:47 +0530

    while do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// let i = 0;
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+let i = 10;
+while (i >= 0) {
+//   console.log(i);
+  i--;
+}
+let myArr =["super","hero","spider"]
+
+let arr = 0
+while(arr<myArr.length){
+    // console.log(`value is ${myArr[arr]}`);
+arr = arr+1
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
+console.log(score);
